3e8822f3671eea9f8bc424de8413e42d7f334452
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-11-19 14:50:49 +0700

    add new plugin

---
+++ wp-content/plugins/templates-patterns-collection/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/templates-patterns-collection',
+        'pretty_version' => '1.2.13',
+        'version' => '1.2.13.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/templates-patterns-collection' => array(
+            'pretty_version' => '1.2.13',
+            'version' => '1.2.13.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.37',
+            'version' => '3.3.37.0',
+            'reference' => '73e9010fb4e30ad8835f00a0df802733f98c4094',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
